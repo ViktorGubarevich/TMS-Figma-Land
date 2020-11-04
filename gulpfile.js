a5ca5e6b02e65902d769992b1f2e2eac96d9c38b
Viktor Gubarevich <gubarew.v@gmail.com>
2020-11-04 18:53:16 +0300

    Changed project TMS-Figma-Land

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,78 @@
-const gulp = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
-const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
 const sass = require('gulp-sass');
-
-var src = {
-	html: 'app/**/*.html',
-	scss: 'app/scss/style.scss',
-	fonts: 'app/fonts/**/*.*',
-	images: 'app/images/**/*.*',
-	js: 'app/js'
-};
-
-var dist = {
-	root: 'dist/',
-	css: 'dist/css/',
-	js: 'dist/js',
-	images: 'dist/images',
-	fonts: 'dist/fonts',
-}
-
-var watch = {
-	scss: 'app/scss/**/*.scss',
-	js: 'app/js/**/*.js',
-	html: 'app/**/*.html',
-	images: 'app/images/**/*.*'
-}
-
-gulp.task('run',
-  [
-    'concat-min-js',
-    'html',
-    'fonts-dist',
-		'img-min',
-		'watch'	
-  ], function() {
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+  
+function browsersync() {
   browserSync.init({
-    server: "./dist"
-  });
-});
+    server: { baseDir: 'app/' },
+    notify: false,
+    online: true
+  })
+}
+  
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'app/js/app.js',
+    ])
+  .pipe(concat('app.min.js'))
+  .pipe(uglify())
+  .pipe(dest('app/js/'))
+  .pipe(browserSync.stream())
+}
+  
+function styles() {
+  return src('app/scss/style.scss')
+  .pipe(sass())
+  .pipe(concat('style.min.css'))
+  .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+  .pipe(cleancss( { level: { 1: { specialComments: 0 } } } ))
+  .pipe(dest('app/css/'))
+  .pipe(browserSync.stream())
+}
+  
+function images() {
+  return src('app/images/**/*')
+  .pipe(imagemin())
+  .pipe(dest('app/images/'))
+}
 
-gulp.task("watch", ["watch:scss", "watch:html", "watch:js", "watch:img"]);
-
-gulp.task('watch:scss', function(){
-	gulp.watch(watch.scss, ['scss']);
-});
-
-gulp.task('watch:html', function(){
-	gulp.watch(watch.html, ['html']);
-});
-
-gulp.task('watch:js', function(){
-	gulp.watch(watch.js, ['concat-min-js']);
-});
-
-gulp.task('watch:img', function(){
-	gulp.watch(watch.images, ['img-min']);
-});
-
-gulp.task('html', function () {
-	gulp.src(src.html)
-		.pipe(gulp.dest(dist.root))
-		.pipe(browserSync.stream());
-});
-
-// Converting scss files to css files (add prefixes, minifyCSS)
-gulp.task('scss', function(){
-	gulp.src(src.scss)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(cleanCSS({level: {1: {specialComments: 0}}}))
-		.pipe(gulp.dest(dist.css))
-		.pipe(browserSync.stream());  
-});
-
-// js concat and min
-gulp.task('concat-min-js', function() {
-	return gulp.src
-		([ src.js+'/**/*.js' ])
-		.pipe(gulp.dest(dist.js))
-		.pipe(browserSync.stream());
-});
-
-// img minimization
-gulp.task('img-min', function() {
-	gulp.src(src.images)
-	.pipe(imagemin())
-	.pipe(gulp.dest(dist.images))
-	.pipe(browserSync.stream());
-});
-
-gulp.task('fonts-dist', function() {
-  	gulp.src(src.fonts)
-	.pipe(gulp.dest(dist.fonts))
-	.pipe(browserSync.stream());
-});
-
-gulp.task('default', ['run']);
+function cleanimg() {
+  return del('app/images/**/*', { force: true })
+}
+  
+function buildcopy() {
+  return src([
+    'app/css/**/*.min.css',
+    'app/js/**/*.min.js',
+    'app/images/**/*',
+    'app/**/*.html',
+    'app/fonts/**/*'
+    ], { base: 'app' })
+  .pipe(dest('dist'))
+}
+  
+function cleandist() {
+  return del('dist/**/*', { force: true })
+}
+  
+function startwatch() {
+  watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+  watch('app/**/scss/**/*', styles);
+  watch('app/**/*.html').on('change', browserSync.reload);
+  watch('app/images/**/*', images);
+}
+  
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
+exports.build = series(cleandist, styles, scripts, buildcopy);
+exports.default = parallel(styles, scripts, browsersync, startwatch);
